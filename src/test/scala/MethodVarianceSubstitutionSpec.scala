3d6593d74a9ec2399cf7822b4e97c0e288bceb08
Rebekah <rwaterbury@gmail.com>
2023-05-25 01:34:05 -0700

    functions and method variance rules

---
+++ src/test/scala/MethodVarianceSubstitutionSpec.scala
@@ -0,0 +1,47 @@
+package org.rwaterbury.scaladev
+
+class Jacket {
+  def genericWashingInstructions = "please read label"
+}
+
+class FauxFirJacket extends Jacket {
+  def specificWashingInstructions = "dry clean only"
+}
+
+object PlayingWithMethodSubstitution {
+  def aMethodWithTheExpectedTypes(fauxFirJacket: FauxFirJacket): AnyRef = {
+    fauxFirJacket.specificWashingInstructions
+  }
+
+  def aMethodWithOtherTypes(jacket: Jacket): String = {
+    jacket.genericWashingInstructions
+  }
+
+  def takesAMethod(fauxFirJacket: FauxFirJacket, substitutable: FauxFirJacket => AnyRef) = {
+    substitutable(fauxFirJacket)
+  }
+}
+
+class MethodVarianceSubstitutionSpec extends FreeSpecStyle {
+  val fauxFirJacket = new FauxFirJacket
+  "running with a method defined to take the stated parameter type" - {
+    "and return the expected return type" - {
+      "will work" in {
+        PlayingWithMethodSubstitution.takesAMethod(
+          fauxFirJacket,
+          PlayingWithMethodSubstitution.aMethodWithTheExpectedTypes
+        ) shouldBe "dry clean only"
+      }
+    }
+  }
+  "running with a method defined to take a supertype of the parameter type" - {
+    "and return a subtype of expected the return type" - {
+      "will work" in {
+        PlayingWithMethodSubstitution.takesAMethod(
+          fauxFirJacket,
+          PlayingWithMethodSubstitution.aMethodWithOtherTypes
+        ) shouldBe "please read label"
+      }
+    }
+  }
+}
